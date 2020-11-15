fa5ef9d0b015be6158a9b9c1a79fd9c551df3f49
YYYlh <1184262331@qq.com>
2020-11-15 19:19:51 +0800

    芜湖

--- vue/响应式原理/index.js
+++ vue/响应式原理/index.js
@@ -1,6 +1,17 @@
 const data = {
     name: 'liuhao',
-    age: 18
+    age: 18,
+    firends: ['小明', '小红']
+}
+
+const oldArrayPortotype = Array.prototype
+const newArrayPrototype = Object.create(oldArrayPortotype)
+
+for (const methodName of ['push', 'pop', 'shift', 'unshift', 'splice']) {
+    newArrayPrototype[methodName] = function() {
+        console.log('视图更新了')
+        oldArrayPortotype[methodName].call(this, ...arguments)
+    }
 }
 
 observe(data)
@@ -9,14 +20,19 @@ function observe(target) {
     if (typeof target !== 'object' || target === null) {
         return target
     }
+    if (Array.isArray(target)) {
+        target.__proto__ = newArrayPrototype
+    }
     for (const key in target) {
         defineReactive(target, key, target[key])
     }
 }
 
 function defineReactive(target, key, value) {
+    observe(value)
     Object.defineProperty(target, key, {
         set(newValue) {
+            observe(newValue)
             if (newValue !== value) {
                 value = newValue
                 console.log('视图更新了')
@@ -27,5 +43,9 @@ function defineReactive(target, key, value) {
         }
     })
 }
-data.age = 10
-console.log(data.age);
+// data.age = 10
+data.firends[1] = '小张'
+data.firends.push('小李')
+console.log(data.firends[0])
+console.log(data.firends[1])
+console.log(data.firends[2])
