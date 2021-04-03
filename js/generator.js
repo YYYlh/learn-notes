336c56337bb94350f1a9282b2b74a79841615e78
YYYlh <1184262331@qq.com>
2021-04-03 22:58:35 +0800

    看不懂艹

---
+++ js/generator.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(id) {
+        this.id = id
+        this.neighbors = new Set()
+    }
+    
+    connect(node) {
+        if (node !== this) {
+            this.neighbors.add(node)
+            node.neighbors.add(this)
+        }
+    }
+}
+
+class RandomGraph {
+    constructor(size) {
+        this.nodes = new Set()
+
+        for (let i = 0; i < size; i++) {
+            this.nodes.add(new Node(i))
+        }
+        const threshold = 1 / size
+        for (const x of this.nodes) {
+            for (const y of this.nodes) {
+                if (Math.random() < threshold) {
+                    x.connect(y)
+                }
+            }
+        }
+    }
+    isConnected() {
+        const visitedNodes = new Set()
+        function * traverse(nodes) {
+            for (const node of nodes) {
+                if (!visitedNodes.has(node)) {
+                    yield node
+                    yield * traverse(node.neighbors)
+                }
+            }
+        }
+        const firstNode = this.nodes[Symbol.iterator]().next().value
+        for (const node of traverse([firstNode])){ 
+            visitedNodes.add(node)
+        }
+        return visitedNodes.size === this.nodes.size
+    }
+}
+
+const g = new RandomGraph(6)
+console.log(g.isConnected());
